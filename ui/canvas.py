98fbf1fe66426ad7c6c0b645d76650e2c3aafa6e
Atick Faisal <atickfaisal@gmail.com>
2021-03-06 21:29:37 +0600

    plot

--- ui/canvas.py
+++ ui/canvas.py
@@ -4,7 +4,7 @@ from .plot import Plot
 
 class Canvas:
 
-    def __init__(self, size=(600, 400), background=(52, 52, 59)):
+    def __init__(self, size=(900, 600), background=(52, 52, 59)):
         self.plt = Plot(x=100, y=100, width=400, height=300)
         if len(size) != 2:
             raise TypeError("size param must be a tuple: (width, height)")
@@ -26,13 +26,10 @@ class Canvas:
                    headless=False)
 
         # set_render_mode(RenderMode.RENDER_MANUAL)
+        set_caption("Gesture Data Visualization")
         self.apply_background()
         self.loop()
         close_graph()
-
-    def loop(self):
-        while is_run():
-            self.plt.bar([1, 2, 3], [100, 30, 200])
 
     def run(self):
         easy_run(self.create)
@@ -41,3 +38,7 @@ class Canvas:
         set_fill_color(self.background_color)
         fill_rect(0, 0, self.width, self.height)
 
+
+    def loop(self):
+        while is_run():
+            self.plt.bar([1, 2, 3], [100, 30, 200])
